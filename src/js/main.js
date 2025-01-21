fa98fa4f2bfd3b0b84bf619fdd50fb3e238056eb
Vita Koval <mayevit@hotmail.com>
2025-01-21 16:16:10 -0700

    create header

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,15 @@
-console.log('main.js');
+//Header burger
+const burger = document.querySelector(".js-burger");
+const header = document.querySelector(".js-header");
+const body = document.querySelector("body");
+
+burger.addEventListener("click", function () {
+    burger.classList.toggle("active");
+    header.classList.toggle("open-menu");
+
+    if (burger.classList.contains("active")) {
+        body.style.overflow = "hidden"; 
+    } else {
+        body.style.overflow = "visible"; 
+    }
+});
