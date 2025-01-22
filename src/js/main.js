463f6c7394d2a7f54a4aab0ee70e5af91a424216
Vita Koval <mayevit@hotmail.com>
2025-01-22 11:40:18 -0700

    implemented adaptive design for header

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,12 @@ const burger = document.querySelector(".js-burger");
 const header = document.querySelector(".js-header");
 const body = document.querySelector("body");
 
+const actions = document.querySelector('.header__action');
+const navigation = document.querySelector('.header__nav');
+const wrapper = document.querySelector('.header__inner');
+
+
+// Toggle burger menu
 burger.addEventListener("click", function () {
     burger.classList.toggle("active");
     header.classList.toggle("open-menu");
@@ -13,3 +19,20 @@ burger.addEventListener("click", function () {
         body.style.overflow = "visible"; 
     }
 });
+
+// Move actions based on screen width
+function moveActions() {
+    if (window.innerWidth < 1072) {
+        if (navigation && !navigation.contains(actions)) {
+            navigation.append(actions);
+        }
+    } else {
+        if (wrapper && !wrapper.contains(actions)) {
+            wrapper.append(actions);
+        }
+    }
+}
+
+// Initial check and on resize
+moveActions(); // Run on load
+window.addEventListener("resize", moveActions); // Run on resize
