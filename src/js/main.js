657969d8f6952c414fbdfdb23b621ed77ef43801
Vita Koval <mayevit@hotmail.com>
2025-01-23 11:45:36 -0700

    added js to submit the form

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+"use strict";
 //Header burger
 const burger = document.querySelector(".js-burger");
 const header = document.querySelector(".js-header");
@@ -36,3 +37,86 @@ function moveActions() {
 // Initial check and on resize
 moveActions(); // Run on load
 window.addEventListener("resize", moveActions); // Run on resize
+
+
+//Submit form
+var formTitle = document.querySelector(".form__title");
+var form = document.querySelector(".form__primary");
+var submitButton = form.querySelector('button[type="submit"]');
+var notification = document.querySelector(".notification");
+
+var fields = [{
+  id: "#email",
+  required: true
+}, {
+  id: "#phone",
+  required: false
+    }, {
+    id: "#date",
+    required: true
+//   id: ".custom-select",
+//   type: "custom-select",
+//   required: true,
+//   defaultValue: "Select..."
+}];
+var handleEmptyField = function handleEmptyField(field) {
+   
+  var element = form.querySelector(field.id);
+//   var value = field.type === "custom-select" ? element.querySelector("select").value : element.value.trim();
+    var value = element.value.trim();
+    if (field.required) {
+    // if (!value || field.defaultValue && value === field.defaultValue) {
+       if (!value) {
+      element.classList.add("empty-field");
+    } else {
+      element.classList.remove("empty-field");
+    }
+  }
+};
+var handleSubmitForm = function handleSubmitForm(evt) {
+    evt.preventDefault();
+  var hasEmptyFields = false;
+    fields.forEach(function (field) {
+      handleEmptyField(field);
+     
+    if (field.required) {
+    //   var value = field.type === "custom-select" ? form.querySelector(field.id).querySelector("select").value : form.querySelector(field.id).value.trim();
+      var value = form.querySelector(field.id).value.trim();
+        if (!value || field.defaultValue && value === field.defaultValue) {
+        hasEmptyFields = true;
+      }
+    }
+  });
+  if (hasEmptyFields) {
+    return;
+  }
+  var email = form.querySelector("#email").value;
+  var phone = form.querySelector("#phone").value;
+  var dateOfAppointment = form.querySelector("#date").value;
+
+  var formData = {
+    email: email,
+    phone: phone,
+    dateOfAppointment: dateOfAppointment,
+  };
+    
+  fetch("https://", {
+    method: "POST",
+    body: JSON.stringify(formData),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  }).then(function () {
+      form.reset();
+      formTitle.classList.add("hidden");
+    form.classList.add("hidden");
+    notification.classList.add("show");
+  }).catch(function () {
+      // Temporary solution
+      form.reset();
+      formTitle.classList.add("hidden");
+      form.classList.add("hidden");
+      notification.classList.add("show");
+    });
+};
+submitButton.addEventListener("click", handleSubmitForm);
